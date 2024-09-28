bad176e9a7f018bc2dc9f157172a4d39fe560d33
Bhekisile <bhekisilejozi@gmail.com>
2024-09-28 11:07:27 +0200

    Update seed and ReadMe file

--- README.md
+++ README.md
@@ -87,6 +87,7 @@ Install this project with:
 bundle install
 rails db:create
 rails db:migrate
+rails db:seed
 
 ### Usage
 
--- app/javascript/store/greetings/greetingSlice.js
+++ app/javascript/store/greetings/greetingSlice.js
@@ -2,19 +2,14 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchGreetings = createAsyncThunk('greetings/fetch', async () => {
   try {
-    const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=faith',
-      {
-        headers: {
-          'X-Api-Key': '5IxClj7l7PEht3AA8nJbiQ==CjzPZ7VSPNDaBiwc',
-        },
-      });
+    const response = await fetch('/api/greetings/random');
 
     if (!response.ok) {
       throw new Error(`Failed to fetch data. Status: ${response.status}`);
     }
 
     const data = await response.json();
-    return data[0];
+    return data;
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error;
@@ -38,8 +33,7 @@ const greetingSlice = createSlice({
         return newState;
       })
       .addCase(fetchGreetings.fulfilled, (state, action) => {
-        const newState = { ...state, greeting: action.payload, loading: false };
-        return newState;
+        return { ...state, greeting: action.payload.message, loading: false };
       })
 
       .addCase(fetchGreetings.rejected, (state) => {
